85e535541158c1cee2852308c19b5d9021faf7ba
Mounadharshini <dhachumaa182@gmail.com>
2026-03-28 09:34:28 +0530

    Fix formatting and padding in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,7 @@ def extract_features(file, max_pad_len=200):
         mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=40)
         if mfcc.shape[1] < max_pad_len:
             pad_width = max_pad_len - mfcc.shape[1]
-            mfcc = np.pad(mfcc, ((0,0),(0,pad_width)), mode='constant')
+            mfcc = np.pad(mfcc, ((0, 0), (0, pad_width)), mode='constant')
         else:
             mfcc = mfcc[:, :max_pad_len]
         return mfcc
