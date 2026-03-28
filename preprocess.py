76a3bf0c2b12008f35c87edaaca446f3106701fc
Mounadharshini <dhachumaa182@gmail.com>
2026-03-28 19:54:24 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,18 +2,24 @@ import librosa
 import numpy as np
 
 def extract_features(file, max_pad_len=200):
-    """
-    Extract MFCC features from audio file
-    """
     try:
+
         audio, sr = librosa.load(file, duration=3)
+
         mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=40)
-        if mfcc.shape[1] < max_pad_len:
-            pad_width = max_pad_len - mfcc.shape[1]
-            mfcc = np.pad(mfcc, ((0, 0), (0, pad_width)), mode='constant')
+
+        chroma = librosa.feature.chroma_stft(y=audio, sr=sr)
+
+        features = np.vstack((mfcc, chroma))
+
+        if features.shape[1] < max_pad_len:
+            pad_width = max_pad_len - features.shape[1]
+            features = np.pad(features, ((0,0),(0,pad_width)), mode='constant')
         else:
-            mfcc = mfcc[:, :max_pad_len]
-        return mfcc
+            features = features[:, :max_pad_len]
+
+        return features
+
     except Exception as e:
         print("Error extracting features:", e)
         return None
